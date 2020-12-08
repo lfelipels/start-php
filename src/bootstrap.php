2710ef7f9d2160f23458b67ed0cba71d226dd4f1
Luis Felipe <luisfelipelimadasilva@gmail.com>
2020-12-08 15:14:25 -0300

    inicializa a  aplicação

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,22 @@
 <?php
 
+use Slim\Factory\AppFactory;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 //configs
 
+$app = AppFactory::create();
+
+//create routes
+include __DIR__ . '/../routes/admin.php';
+
 //start app
+$app->run();
 
 
 
