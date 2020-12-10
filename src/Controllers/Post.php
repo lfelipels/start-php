64dede004313c7698b62deb8f325ff0e8d79cc0e
Luis Felipe <luisfelipelimadasilva@gmail.com>
2020-12-10 19:14:31 -0300

    create post controller

--- src/Controllers/Post.php
+++ src/Controllers/Post.php
@@ -3,11 +3,24 @@
 namespace App\Controllers;
 
 use App\Controllers\Base;
+use App\Core\Database\Connection;
+use App\Repositories\Post as PostRepository;
 
 class Post extends Base
 {
+
+    private $postRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->postRepository = new PostRepository(Connection::getInstance());
+    }
+
     public function index()
     {
-        $this->view->render('post.index');
+        $this->view->render('post.index', [
+            'posts' => $this->postRepository->findBy('p.author', 1)
+        ]);
     }
 }
