e6efc5187b4159ce2fe638f1dde277eaf0bde6c4
Luis Felipe <luisfelipelimadasilva@gmail.com>
2020-12-10 20:35:41 -0300

    update post view

--- views/post/index.php
+++ views/post/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,23 +8,32 @@
 
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <!-- form-post -->
     <section class="post">
-        <form action="" class="post__form">
+        <form action="/admin/posts" class="post__form" method="POST">
             <header class="post__header">
 
                 <div class="post_actions">
                     <a href="#">Visualizar</a>
                     <button type="submit">Salvar</button>
                 </div>
-                
-            </header>        
+
+            </header>
+            <label for="category">
+                <select name="category" id="category">
+                    <option value="">Selecione uma categoria</option>
+                    <?php foreach ($categories as $category) : ?>
+                        <option value="<?= $category->id() ?>"> <?= $category->description()?></option>
+                    <?php endforeach ?>
+                </select>
+            </label>
             <label for="title">
                 <span>Title:</span>
                 <input type="text" name="title" id="title">
             </label>
-            <label for="content"> 
+            <label for="content">
                 <span>Conteúdo:</span>
                 <textarea name="content" id="content" cols="30" rows="20"></textarea>
             </label>
@@ -33,27 +43,28 @@
         </form>
     </section>
 
-    
+
     <!-- list-post -->
-    <?php if(empty($posts)):?>
+    <?php if (empty($posts)) : ?>
         <div class="post__list empty">
             Você ainda não criou nenhuma postagem. Crie agora mesmo.
         </div>
-    <?php else: ?>
-        <?php foreach($posts as $post): ?> 
+    <?php else : ?>
+        <?php foreach ($posts as $post) : ?>
             <article class="post__card">
                 <div class="post__card__header">
-                    <h3><?=$post->title()?></h3>
-                    <span><?=$post->author()->name()?></span>
+                    <h3><?= $post->title() ?></h3>
+                    <span><?= $post->author()->name() ?></span>
                     <strong><?= $post->category()->parent() ? $post->category()->description()->parent()->description() . '>>' : '' . $post->category()->description() ?></strong>
-                    <small>Publicado em: <?=$post->createdAt()->format('d/m/Y \à\s H:m:i')?></small>
+                    <small>Publicado em: <?= $post->createdAt()->format('d/m/Y \à\s H:m:i') ?></small>
                 </div>
                 <p class="post__card__body">
-                    <?=$post->content()?>
+                    <?= $post->content() ?>
                 </p>
             </article>
-        <?php endforeach ?>     
+        <?php endforeach ?>
     <?php endif ?>
-    
+
 </body>
+
 </html>
