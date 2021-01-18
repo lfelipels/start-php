1c1e202d1eaf8554ea2fa77c5896bf6ae2302a7b
Luis Felipe <luisfelipelimadasilva@gmail.com>
2021-01-18 11:55:51 -0300

    update Redirect class

--- src/Helpers/Redirect.php
+++ src/Helpers/Redirect.php
@@ -2,12 +2,40 @@
 
 namespace App\Helpers;
 
-use Psr\Http\Message\ResponseInterface;
+use App\Core\Http\Request;
+use App\Core\Session\FlashMessage;
+use App\Core\Validation\ErrorValidation;
 
 class Redirect
 {
     public static function to(string $url)
     {
         return header('Location:'. $url);
+    }
+    
+    /**
+     * Add data as flash message
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return self
+     */
+    public static function with(string $key, $value)
+    {
+        (new FlashMessage)->set($key, $value);
+        return new static;
+    }
+
+    public static function withErrors(array $errors)
+    {
+        (new FlashMessage)->set('errors', new ErrorValidation($errors));
+        return new static;
+    }
+    
+    public static function back()
+    {
+        $request = new Request();
+        $dados = $request->getServer();
+        return header('Location:'. $dados['HTTP_REFERER']);
     }
 }
